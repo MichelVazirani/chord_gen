1fcced465b64747917b469e80605cdd51a563b0a
MichelVazirani <michel.vazirani@gmail.com>
2022-02-13 21:28:25 -0500

    Node

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('.'));
+
+app.get("", (req, res) => {
+  res.sendFile("index.html", { root: '.' });
+});
+
+app.listen(PORT, () => console.info(`App listening on port ${PORT}`))
